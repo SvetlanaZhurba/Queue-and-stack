3e8fa199992ba19501598008b4a7107121c62d8f
SvetlanaZhurba <43250637+SvetlanaZhurba@users.noreply.github.com>
2019-05-15 19:56:09 +0300

    Update list.h

--- list.h
+++ list.h
@@ -10,29 +10,3 @@ public:
   virtual void show();
   virtual void push( int n );
 };
-
-class stack : public list
-{
-private:
-  node *head;
-
-public:
-  stack();
-  ~stack();
-  int pop();
-  void show();
-  void push( int n );
-};
-
-class queue : public list
-{
-private:
-  node *end;
-
-public:
-  queue();
-  ~queue();
-  int pop();
-  void show();
-  void push( int n );
-};
